226d9f53dc926ba8c900a05c0c29b71990307ec3
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-09-20 13:11:09 -0400

    Set class names

--- app/src/main/java/ajlyfe/lectureapp/ClassMainScreen.java
+++ app/src/main/java/ajlyfe/lectureapp/ClassMainScreen.java
@@ -3,11 +3,11 @@ package ajlyfe.lectureapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class ClassMainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.ClassMainScreen);
     }
 }
