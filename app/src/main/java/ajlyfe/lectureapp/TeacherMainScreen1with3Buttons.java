b64eeb0e32ed15ef1607196f6c775d9ba648177a
IcePhoenix2 <adv_java@CHS-Room-143-Student-03.local>
2016-09-20 13:17:21 -0400

    Create main screen for teachers
    THERE ARE YOU HAPPY KEVIN?!?!?! PRESENT TENSE!!!!!!!!!

---
+++ app/src/main/java/ajlyfe/lectureapp/TeacherMainScreen1with3Buttons.java
@@ -0,0 +1,13 @@
+package ajlyfe.lectureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherMainScreen1with3Buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_main_screen1with3_buttons);
+    }
+}
