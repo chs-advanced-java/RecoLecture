e045a7ac8a6c33438f24ebce67fbbe9d0ebab244
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-09-20 14:05:11 -0400

    Fixes name

--- app/src/main/java/ajlyfe/lectureapp/TeacherClassViewScreen.java
+++ app/src/main/java/ajlyfe/lectureapp/TeacherClassViewScreen.java
@@ -8,6 +8,6 @@ public class TeacherClassViewScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_class_view_screen);
+        setContentView(R.layout.teacher_class_view_screen);
     }
 }
