24112b40245539e86cfdbc8b4abcb26275634864
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-09-20 14:04:06 -0400

    Makes subactivity for teacherclassmainscreen

---
+++ app/src/main/java/ajlyfe/lectureapp/TeacherClassViewScreen.java
@@ -0,0 +1,13 @@
+package ajlyfe.lectureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherClassViewScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_class_view_screen);
+    }
+}
