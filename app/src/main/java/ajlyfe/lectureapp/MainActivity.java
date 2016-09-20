a0d3e969ad3358c385d895caa25e79378a179038
Kevin Dixson <kdixson538@gmail.com>
2016-09-20 14:19:28 -0400

    Confirm upload slide

--- app/src/main/java/ajlyfe/lectureapp/MainActivity.java
+++ app/src/main/java/ajlyfe/lectureapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
--- app/src/main/java/ajlyfe/lectureapp/TeacherMainScreen1with3Buttons.java
+++ app/src/main/java/ajlyfe/lectureapp/TeacherMainScreen1with3Buttons.java
@@ -13,16 +13,4 @@ public class TeacherMainScreen1with3Buttons extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_main_screen1with3_buttons);
     }
-
-    Button btn = (Button) findViewById(R.id.buttonClasses);
-    /*
-    Button btn = (Button) findViewById(R.id.);
-
-    btn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(TeacherMainScreen1with3Buttons.this, TeacherClassMainScreen.class));
-        }
-    });
-    */
 }
