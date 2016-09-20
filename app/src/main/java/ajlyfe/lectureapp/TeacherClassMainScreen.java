5f30c8bd5258264a6664757ee612ec0c1eb2d5c8
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-09-20 13:28:19 -0400

    Changes activity names to include "teacher"
    For continuity’s sake

--- app/src/main/java/ajlyfe/lectureapp/TeacherClassMainScreen.java
+++ app/src/main/java/ajlyfe/lectureapp/TeacherClassMainScreen.java
@@ -3,11 +3,11 @@ package ajlyfe.lectureapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ClassMainScreen extends AppCompatActivity {
+public class TeacherClassMainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.class_main_screen);
+        setContentView(R.layout.teacher_class_main_screen);
     }
 }
