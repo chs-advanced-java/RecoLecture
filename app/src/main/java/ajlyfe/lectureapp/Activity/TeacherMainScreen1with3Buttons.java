3b3e0362505af6d18d1231d8c681fbaa98902709
IcePhoenix2 <adv_java@CHS-Room-143-Student-03.local>
2016-09-22 14:03:00 -0400

    Gets the button working!!!!!!
    Thank you Kevin, or should I say Elliot 1.5?

--- app/src/main/java/ajlyfe/lectureapp/Activity/TeacherMainScreen1with3Buttons.java
+++ app/src/main/java/ajlyfe/lectureapp/Activity/TeacherMainScreen1with3Buttons.java
@@ -14,5 +14,15 @@ public class TeacherMainScreen1with3Buttons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_main_screen1with3_buttons);
+
+        Button btn = (Button) findViewById(R.id.buttonClasses);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(TeacherMainScreen1with3Buttons.this, TeacherClassMainScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
