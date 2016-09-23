75bfe88f10a9d1e88048853a66ae2480736e0ada
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-09-23 13:25:51 -0400

    Makes second button and java's it

--- app/src/main/java/ajlyfe/lectureapp/Activity/TeacherClassMainScreen.java
+++ app/src/main/java/ajlyfe/lectureapp/Activity/TeacherClassMainScreen.java
@@ -15,8 +15,18 @@ public class TeacherClassMainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_class_main_screen);
 
-        Button classMainScreenButton = (Button) findViewById(R.id.buttonClassesViewTest);
-        classMainScreenButton.setOnClickListener(new View.OnClickListener(){
+        Button classMainButtonOne = (Button) findViewById(R.id.buttonClassesTestOne);
+        classMainButtonOne.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(TeacherClassMainScreen.this, TeacherClassViewScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        Button classMainButtonTwo = (Button) findViewById(R.id.buttonClassesTestTwo);
+        classMainButtonTwo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v)
             {
