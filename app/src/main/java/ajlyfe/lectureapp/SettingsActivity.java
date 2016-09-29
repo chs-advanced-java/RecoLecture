4a3aa1c7ef0742f00de3d9b9034508a6510d40d8
Punchieo <kdixson538@gmail.com>
2016-09-28 22:11:14 -0400

    Purge ugly FAB (2/2)
    oops

--- app/src/main/java/ajlyfe/lectureapp/SettingsActivity.java
+++ app/src/main/java/ajlyfe/lectureapp/SettingsActivity.java
@@ -15,15 +15,5 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
