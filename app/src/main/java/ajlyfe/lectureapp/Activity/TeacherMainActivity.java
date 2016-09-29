0e04e76cb33c9c4806e042aeeb1c16e8181af10a
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-09-29 13:07:30 -0400

    Removes pointless button
    Warning: pointless

--- app/src/main/java/ajlyfe/lectureapp/Activity/TeacherMainActivity.java
+++ app/src/main/java/ajlyfe/lectureapp/Activity/TeacherMainActivity.java
@@ -22,15 +22,6 @@ public class TeacherMainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         CardView myClassesCard = (CardView) findViewById(R.id.buttonClasses);
         myClassesCard.setOnClickListener(new View.OnClickListener(){
             @Override
