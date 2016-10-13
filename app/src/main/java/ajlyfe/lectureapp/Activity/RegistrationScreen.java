5f5ce806222b521744e8ca826704e5e5b733a384
Aragolt <adv_java@CHS-Room-143-Student-04.local>
2016-10-13 13:31:14 -0400

    ACTUALLY does the things the last edit was supposed to do

--- app/src/main/java/ajlyfe/lectureapp/Activity/RegistrationScreen.java
+++ app/src/main/java/ajlyfe/lectureapp/Activity/RegistrationScreen.java
@@ -1,11 +1,13 @@
 package ajlyfe.lectureapp.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import ajlyfe.lectureapp.R;
 
@@ -18,7 +20,13 @@ public class RegistrationScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        Button register = (Button) findViewById(R.id.buttonRegister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationScreen.this, LoginActivity.class));
+            }
+        });
     }
 
 }
