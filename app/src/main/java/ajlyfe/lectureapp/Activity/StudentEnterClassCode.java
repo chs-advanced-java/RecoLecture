6dee747f71f88bb11094a2c584742d56b987b971
IcePhoenix2 <adv_java@CHS-Room-143-Student-03.local>
2016-10-17 13:25:16 -0400

    Makes it impossible to join a class without the right key (3zb8c27n)

--- app/src/main/java/ajlyfe/lectureapp/Activity/StudentEnterClassCode.java
+++ app/src/main/java/ajlyfe/lectureapp/Activity/StudentEnterClassCode.java
@@ -3,18 +3,25 @@ package ajlyfe.lectureapp.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StringDef;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import ajlyfe.lectureapp.R;
 
+import static android.R.id.edit;
+
 public class StudentEnterClassCode extends AppCompatActivity {
+
 
 
 
@@ -31,8 +38,16 @@ public class StudentEnterClassCode extends AppCompatActivity {
         joinClassButton.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View view) {
-                startActivity(new Intent(StudentEnterClassCode.this,StudentClassPage.class ));
-                finish();
+                final EditText enterClassCode =  (EditText) findViewById(R.id.editText3);
+                String classCodeStudent = (String) enterClassCode.getText().toString();
+                if (classCodeStudent.equals("3zb8c27n")) {
+                    startActivity(new Intent(StudentEnterClassCode.this, StudentClassPage.class));
+                    finish();
+
+                }
+                else {
+                    Toast.makeText(StudentEnterClassCode.this, "Your code does not appear to be valid. Please check it and try again.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
