8d909903072ed029f37c317ba42e3fe02754d2d4
Aragolt <sttopper>
2017-01-25 13:58:31 -0500

    Minor Clean of Fragments

--- app/src/main/java/ajlyfe/lectureapp/Activity/TeacherClassView.java
+++ app/src/main/java/ajlyfe/lectureapp/Activity/TeacherClassView.java
@@ -36,8 +36,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
